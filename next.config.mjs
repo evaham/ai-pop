5209a7e4b89dceed00f27c9c492cbe8d78ed2cb0
JungWoo Ham <evaham@naver.com>
2026-02-23 16:30:37 +0900

    github 폴더 주소 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = "ai-pop";
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  /* config options here */
+	basePath: isProd ? `/${repoName}` : "",
+	assetPrefix: isProd ? `/${repoName}` : "",
+	env: {
+		NEXT_PUBLIC_BASE_PATH: isProd ? `/${repoName}` : "",
+	},
 };
 
 export default nextConfig;
